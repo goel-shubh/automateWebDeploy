734e5089013f889476134f26263516e81e6e21bf
goel-shubh <shubham.goel772@gmail.com>
2018-04-11 16:45:06 +0530

    initialcommiit

---
+++ ARM-01.ps1
@@ -0,0 +1,11 @@
+﻿#$azureAccountName =" enter account name"
+ #$azurePassword = ConvertTo-SecureString "enter Password" -AsPlainText -Force
+
+#$psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
+
+Login-AzureRmAccount 
+
+New-AzureRmResourceGroup -Name 'shuhResource' -Location 'centralindia'
+$resourceGroup = "shuhResource"
+$location = "centralindia"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile "C:\Users\Shubham\Desktop\web1\powershell-master\azuredeploy-app-service-with-git.json"  
